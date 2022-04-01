9d7f51c0c1d3507332d161f0397fd0ba4023686f
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-04-02 04:04:35 +0530

    Dashboard router setup

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 let routerCustomer = require("./customer");
+let routerDashboard = require("./dashboard");
 
+// Customer router
 router.use("/customer", routerCustomer);
+
+// Dashboard router
+router.use("/dasboard", routerCustomer);
 
 module.exports = router;
