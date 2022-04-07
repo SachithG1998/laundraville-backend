5689dc3b20084c60f44200f1ef893d5ba8d87325
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-04-08 03:36:22 +0530

    Returns basket items combined with other collections

--- controllers/basket.js
+++ controllers/basket.js
@@ -1,6 +1,9 @@
 const BasketItemsModel = require("../models/basketItems.model");
+const ServiceModel = require("../models/service.model");
 
-const getBasketItems = (req, res) => {
+const getBasketItems = async (req, res) => {
+  let basketItemsJoined = new Array();
+
   BasketItemsModel.find(
     { basketID: req.params.basketID },
     (err, basketItems) => {
@@ -15,9 +18,36 @@ const getBasketItems = (req, res) => {
           message: "Unable to fetch the basket items at this time",
         });
       } else {
-        res.json({
-          statusMessage: "RETURNED_BASKET_ITEMS",
-          basketItems: basketItems,
+        basketItems.forEach((basketItem, index) => {
+          ServiceModel.findById(
+            basketItem.serviceID,
+            "serviceName image unit",
+            (err, service) => {
+              if (err)
+                return res.json({
+                  statusMessage: "ERROR",
+                  errorMessage: err,
+                });
+              else if (!service) {
+                return res.json({
+                  statusMessage: "NO_BASKET_ITEMS",
+                  message: "Unable to fetch the basket items at this time",
+                });
+              } else {
+                basketItemsJoined[index] = {
+                  ...basketItem._doc,
+                  service: service,
+                };
+
+                if (index === basketItems.length - 1) {
+                  res.json({
+                    statusMessage: "RETURNED_BASKET_ITEMS",
+                    basketItems: basketItemsJoined,
+                  });
+                }
+              }
+            }
+          );
         });
       }
     }
