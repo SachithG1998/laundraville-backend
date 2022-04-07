047ce8ca158661fd7c87f83cb0ce19a5f9825095
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-04-07 08:55:12 +0530

    Customer controller with new request endpoints

--- controllers/customer.js
+++ controllers/customer.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const CustomerModel = require("../models/customer.model");
 
 const registerCustomer = (req, res) => {
-  const found = CustomerModel.find(
+  CustomerModel.find(
     {
       $or: [
         { "login.username": req.body.username },
@@ -16,58 +16,56 @@ const registerCustomer = (req, res) => {
     (err, customer) => {
       if (err) {
         console.log(err);
-      } else {
+      } else if (customer.length) {
         return res.json({
           statusMessage: "CUSTOMER_EXISTS",
           message:
             "You have already registered to Laundraville. Please Log In.",
         });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+          if (err) console.log(err);
+          else {
+            let newCustomer = new CustomerModel({
+              name: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+              },
+              dob: new Date(
+                moment(req.body.dob, "YYYY-MM-DD").format("YYYY/MM/DD")
+              ),
+              address: {
+                line1: req.body.addressLine1,
+                line2: req.body.addressLine2,
+                postalCity: req.body.postalCity,
+                district: req.body.district,
+              },
+              phone: req.body.phone,
+              email: req.body.email,
+              login: {
+                username: req.body.username,
+                password: hashedPassword,
+              },
+            });
+
+            newCustomer.save((err) => {
+              if (err) {
+                res.status(502).json({
+                  error: err.toString(),
+                });
+              } else {
+                res.status(200).json({
+                  id: newCustomer.id,
+                  statusMessage: "CUSTOMER_REGISTERED_SUCCESSFULLY",
+                  message: "Customer registered successfully",
+                });
+              }
+            });
+          }
+        });
       }
     }
   );
-
-  if (!found) {
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-      if (err) res.send(err);
-      else {
-        let newCustomer = new CustomerModel({
-          name: {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-          },
-          dob: new Date(
-            moment(req.body.dob, "YYYY-MM-DD").format("YYYY/MM/DD")
-          ),
-          address: {
-            line1: req.body.addressLine1,
-            line2: req.body.addressLine2,
-            postalCity: req.body.postalCity,
-            district: req.body.district,
-          },
-          phone: req.body.phone,
-          email: req.body.email,
-          login: {
-            username: req.body.username,
-            password: hashedPassword,
-          },
-        });
-      }
-
-      newCustomer.save((err) => {
-        if (err) {
-          res.status(502).json({
-            error: err.toString(),
-          });
-        } else {
-          res.status(200).json({
-            id: newCustomer.id,
-            statusMessage: "CUSTOMER_REGISTERED_SUCCESSFULLY",
-            message: "Customer Registered Successfully",
-          });
-        }
-      });
-    });
-  }
 };
 
 const loginCustomer = (req, res) => {
